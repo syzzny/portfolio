8fffa83363753c574810f49cd0fe2d3e91705388
syzzny <son115502@gmail.com>
2023-06-12 01:41:50 +0900

    add

---
+++ src/pages/MainStylecomp.jsx
@@ -0,0 +1,71 @@
+import styled from 'styled-components'
+import { Icon } from '@iconify/react';
+
+
+
+export const Wrap = styled.div`
+    max-width: 500px;
+    /* background-color: #f5f5f5; */
+    height: 100vh;
+    margin: 0 auto;
+    margin-top: 120px;
+`
+export const ProfileWrap = styled.div`
+    text-align: initial;
+`
+export const Name = styled.p`
+    font-size: 30px;
+    margin: 12px 0;
+    font-weight: 500;
+    margin-bottom: 0px;
+`
+export const Info = styled.p`
+    margin: 14px 0;
+    font-size: 20px;
+    color: #4E4E4E;
+    margin-top: 7px;
+`
+export const Availavle = styled.div`
+    display: flex;
+    align-items: center;
+    height: 29px;
+`
+export const Circle = styled.div`
+    width: 9px;
+    height: 9px;
+    background-color: #60C150;
+    border-radius: 10px;
+`
+export const AvaText = styled.p`
+    margin-left: 10px;
+    color: #4E4E4E;
+`
+export const MyInfo = styled.div`
+    margin-top: 40px;
+    background-color: #FAFAFA;
+    border-radius: 20px;
+    padding: 5px 20px;
+`
+export const MyInfoText = styled.p`
+    font-size: 18px;
+    line-height: 26px;
+    word-break:keep-all;
+    font-family: 'Pretendard';
+`
+export const WorksWrap = styled.p`
+    margin-top: 70px;
+    text-align: initial;
+`
+export const WorksH = styled.div`
+    display: flex;
+    align-items: center;
+`
+export const IconStyle = styled(Icon)`
+    font-size: 22px;
+    color: #737373;
+    margin-right: 10px;
+`
+export const IconText = styled.p`
+    font-size: 18px;
+    color: #737373;
+`
